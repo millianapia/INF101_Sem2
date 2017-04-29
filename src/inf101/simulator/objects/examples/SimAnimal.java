1667d6b9321ef953e42954374b3d08164c77a0b2
milli <milli@10.111.26.193>
2017-04-29 16:32:22 +0200

    Added code for step, small code for repellant

--- src/inf101/simulator/objects/examples/SimAnimal.java
+++ src/inf101/simulator/objects/examples/SimAnimal.java
@@ -9,6 +9,7 @@ import inf101.simulator.objects.AbstractMovingObject;
 import inf101.simulator.objects.IEdibleObject;
 import inf101.simulator.objects.ISimObject;
 import inf101.simulator.objects.SimEvent;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
@@ -24,7 +25,24 @@ public class SimAnimal extends AbstractMovingObject {
 	@Override
 	public void draw(GraphicsContext context) {
 		super.draw(context);
-		context.fillOval(0, 0, getWidth(), getHeight());
+		context.drawImage(MediaHelper.getImage("images/pusheen.png"), 0, 0, getWidth(), getHeight());
+		//double direction = getDirection().toAngle();
+		
+		
+	//	if(direction > -90 && direction <90){
+			
+		//	context.translate(getX(), getY());
+			//context.rotate(direction);
+			//context.scale(1.0, -1.0);
+		//}
+	
+	
+		
+		
+		
+		
+		
+		
 	}
 
 	public IEdibleObject getBestFood() {
@@ -68,18 +86,29 @@ public class SimAnimal extends AbstractMovingObject {
 	for (ISimObject obj : habitat.nearbyObjects(this, getRadius()+400)) {
 			Direction dir1 = this.directionTo(obj);
 			Direction dir2 = obj.getDirection();
-			
-			if(obj instanceof IEdibleObject && (Math.abs(dir1)-dir2)<90){
-				accelerateTo(defaultSpeed, 100);
+						
+			if(obj instanceof IEdibleObject && (Math.abs(dir1.toAngle())-dir2.toAngle())<90){
 				dir = dir.turnTowards(directionTo(obj), 2);
 				if(distanceTo(obj)<2){
 					((IEdibleObject) obj).eat(10);
 					
 				}
 			}
+			else if(obj instanceof SimRepellant && Math.abs(dir1.toAngle())-dir2.toAngle()<90){
+			
+				if(distanceTo(obj)<20){
+					dir = dir.turnTowards(dir.turnBack(), 20);
+					accelerateTo(defaultSpeed, 0.3);	
+
+				}
+				
+			}
 
 	}
-
+	
+	
+	
+		
 		accelerateTo(defaultSpeed, 0.1);
 
 		super.step();
